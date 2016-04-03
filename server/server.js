291f95879a36f54b22b89c81ad256a81ad333a9c
Zsolt Geresdi <tiga80@gmail.com>
2016-04-03 16:20:51 +0000

    modified build config to be compatible with Cloud9 IDE

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import fs from 'fs'
 import http from 'http'
 import express from 'express'
 import path from 'path'
@@ -6,13 +7,24 @@ import chalk from 'chalk'
 const app = express()
 const server = http.Server(app)
 
-const PORT = process.env.PORT || 8081
 const ENV = process.env.NODE_ENV || 'dev'
+const PORT = /*process.env.PORT ||*/ 8080
+const WEBPACK_HOST = ENV == 'dev' ? (process.env.C9_HOSTNAME || process.env.IP || 'localhost') : ''
+const WEBPACK_PORT = ENV === 'dev' ? 8081 : PORT
+
+const indexHtml = new Promise((resolve, reject) => fs.readFile(
+    path.resolve(__dirname, '..', 'assets', 'index.html'), 
+    (err, data) => err ? reject(err) : 
+        resolve(data.toString().split('{HOSTNAME}').join(`//${WEBPACK_HOST}:${WEBPACK_PORT}`))
+))
 
 app.use('/assets', express.static(
     path.resolve(__dirname, '..', 'assets')))
-app.get('/*', (req, res) => res.sendFile(
-    path.resolve(__dirname, '..', 'assets', 'index.html')))
+    
+app.get('/', (req, res) => indexHtml.then(data => {
+    res.set('Content-Type', 'text/html')
+    res.send(data)
+}))
 
 server.listen(PORT)
 console.log(`Server running on port ${chalk.red(PORT)}, environement: ${chalk.blue(ENV)}`)
